4533397869edab7ff475a20578b91de3987b1d3c
3rr0r-4O4 <86462148+3rr0r-4O4@users.noreply.github.com>
2021-06-26 01:46:56 +0600

    Create os.py

---
+++ os.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+os.system("clear")
+try:
+	def _start_():
+  		print()
+
+  			
+  		print("3RR0R-404 IS HERE")
+	def _init_():
+		cli = input("3RR0R-404~# ")
+		os.system(cli)
+		if (cli == "cd"):
+           		dir=input("enter a directory: ")
+           		os.chdir(dir)
+	_start_()
+
+	def lamer():
+    		while True:
+       			_init_()
+	lamer()
+except:
+  print("\nYou are a 3rr0r os user")
